b3e2f5d4191d137dd70b8ad36e53c142d288fd06
truc2004 <nguyenthanhtruc041204@gmail.com>
2025-10-26 20:02:01 +0700

    build apk

--- Lab09/TakeNote/app/_layout.tsx
+++ Lab09/TakeNote/app/_layout.tsx
@@ -1,10 +1,10 @@
 import { Stack } from 'expo-router';
 import { SQLiteProvider } from 'expo-sqlite';
-import { initDb } from '../data/db';
+import { setupDB } from '../data/db';
 
 export default function RootLayout() {
   return (
-    <SQLiteProvider databaseName="tasks.db" onInit={initDb}>
+    <SQLiteProvider databaseName="tasks.db" onInit={setupDB}>
       <Stack screenOptions={{ headerShown: false }}>
 
       </Stack>
